1a0689b4a254bc817a1842c265f8c503ab3b9f70
BdoubleDNG <34476107+BdoubleDNG@users.noreply.github.com>
2019-04-27 19:05:40 +0200

    added PartyList

---
+++ ramme/src/components/activity.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import PartyList from "./partyList";
+class Activity extends Component {
+  render() {
+    return <PartyList />;
+  }
+}
+
+export default Activity;
--- ramme/src/components/main.jsx
+++ ramme/src/components/main.jsx
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 import Action from "./action";
-
+import Activity from "./activity";
 import "../css/main.css";
 
 class Main extends Component {
-    state = {};
+  state = {};
 
-    onButtonClick = () => {
-        console.log("Button clicked! Search for available party and switch view.");
-    };
+  onButtonClick = () => {
+    console.log("Button clicked! Search for available party and switch view.");
+  };
 
-    render() {
-        return (
-            <main>
-                {/* TODO: Add text component here */}
-                <Action onButtonClick={this.onButtonClick} />
-                {/* TODO: Add Activity component here */}
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main>
+        {/* TODO: Add text component here */}
+        <Action onButtonClick={this.onButtonClick} />
+        <Activity />
+      </main>
+    );
+  }
 }
 
 export default Main;
