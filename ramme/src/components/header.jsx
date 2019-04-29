8cc57b439bd149874f73f4846c5df04432c432c4
p-rott <45209365+p-rott@users.noreply.github.com>
2019-04-29 21:22:06 +0200

    Moved option gear over near the center. added small placeholder animation.

--- ramme/src/components/header.jsx
+++ ramme/src/components/header.jsx
@@ -3,15 +3,15 @@ import Logo from "./logo";
 import Option from "./option";
 
 class Header extends Component {
-    state = {};
-    render() {
-        return (
-            <header>
-                <Option />
-                <Logo />
-            </header>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <header>
+        <Option />
+        <Logo />
+      </header>
+    );
+  }
 }
 
 export default Header;
--- ramme/src/components/option.jsx
+++ ramme/src/components/option.jsx
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import { ReactComponent as Logo } from "../images/baseline-settings-20px.svg";
 
 class Option extends Component {
-  state = {};
+  state = { rotate: false };
   render() {
     return (
-      <div className="center">
-        <Logo />
+      <div className="partyList">
+        <Logo
+          onClick={() => this.setState({ rotate: true })}
+          onAnimationEnd={() => this.setState({ rotate: false })}
+          className={this.state.rotate ? "rotate" : ""}
+        />
       </div>
     );
   }
 }
+/*
+        <Logo
+          onClick={() => this.setState({ rotate: true })}
+          onAnimationEnd={() => this.setState({ rotate: false })}
+          className={rotate ? "rotate" : ""}
+        />
+        */
 
 export default Option;
