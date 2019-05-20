7fcd8eab1d079f360861873833f30d008ced932f
BdoubleDNG <34476107+BdoubleDNG@users.noreply.github.com>
2019-05-20 11:49:24 +0200

    Modofied sessions

--- ramme-backend/modules/sessionManager.js
+++ ramme-backend/modules/sessionManager.js
@@ -4,21 +4,22 @@ const uuid = require("uuid/v4");
 function getSession(req, res) {
   fs.readFile("./ressources/sessions.json", (err, data) => {
     let sessions = JSON.parse(data).sessions;
-    if ((session = sessions.find(element => element.id === req.session.id))) {
-      if (req.session.securityToken === session.securityToken)
-        res.send(`Hallo : ${req.session.name}`);
+    if (
+      (session = sessions.find(
+        element => element.securityToken === req.session.securityToken
+      ))
+    ) {
+      console.log(req.session);
+      res.send(req.session);
     } else {
       req.session.securityToken = uuid();
       req.session.name = "test";
-      sessions.push({
-        id: req.session.id,
-        securityToken: req.session.securityToken,
-        name: req.session.name
-      });
+      sessions.push(req.session);
 
       let data = JSON.stringify({ sessions: sessions }, null, 2);
       fs.writeFile("./ressources/sessions.json", data, err => {
         if (err) console.log(err);
+        console.log(req.session);
         res.send(req.session);
       });
     }
