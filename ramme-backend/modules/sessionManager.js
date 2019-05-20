0af28c60bb63532ebf4cc81062f296a32a939726
Marco <>
2019-05-20 11:48:02 +0200

    added random names

--- ramme-backend/modules/sessionManager.js
+++ ramme-backend/modules/sessionManager.js
@@ -1,28 +1,29 @@
 const fs = require("fs");
 const uuid = require("uuid/v4");
 
-function getSession(req, res) {
-  fs.readFile("./ressources/sessions.json", (err, data) => {
-    let sessions = JSON.parse(data).sessions;
-    if ((session = sessions.find(element => element.id === req.session.id))) {
-      if (req.session.securityToken === session.securityToken)
-        res.send(`Hallo : ${req.session.name}`);
-    } else {
-      req.session.securityToken = uuid();
-      req.session.name = "test";
-      sessions.push({
-        id: req.session.id,
-        securityToken: req.session.securityToken,
-        name: req.session.name
-      });
+const randomNameGenerator = require("./randomName");
 
-      let data = JSON.stringify({ sessions: sessions }, null, 2);
-      fs.writeFile("./ressources/sessions.json", data, err => {
-        if (err) console.log(err);
-        res.send(req.session);
-      });
-    }
-  });
+function getSession(req, res) {
+    fs.readFile("./ressources/sessions.json", (err, data) => {
+        let sessions = JSON.parse(data).sessions;
+        if ((session = sessions.find(element => element.id === req.session.id))) {
+            if (req.session.securityToken === session.securityToken) res.send(`Hallo : ${req.session.name}`);
+        } else {
+            req.session.securityToken = uuid();
+            req.session.name = randomNameGenerator.getRandomName();
+            sessions.push({
+                id: req.session.id,
+                securityToken: req.session.securityToken,
+                name: req.session.name
+            });
+
+            let data = JSON.stringify({ sessions: sessions }, null, 2);
+            fs.writeFile("./ressources/sessions.json", data, err => {
+                if (err) console.log(err);
+                res.send(req.session);
+            });
+        }
+    });
 }
 
 exports.getSession = getSession;
