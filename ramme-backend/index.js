0c34eb21507fda39cd45c22b0576b1fe5fcfd11a
BdoubleDNG <34476107+BdoubleDNG@users.noreply.github.com>
2019-05-23 21:52:07 +0200

    added session store in mongodb

--- ramme-backend/index.js
+++ ramme-backend/index.js
@@ -2,40 +2,54 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const temmplateData = require("./ressources/templatedata.json");
 const session = require("express-session");
+var MongoDBStore = require("connect-mongodb-session")(session);
 const sessionManager = require("./modules/sessionManager");
 const chatManager = require("./modules/chatManager");
 const cors = require("cors");
 
 var whitelist = ["http://localhost:3000", "http://m-spreckels.net"];
 var corsOptions = {
-    origin: function(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 };
 
 const app = express();
+var store = new MongoDBStore({
+  uri:
+    "mongodb+srv://root:rammemongo@rammecluster-qhhyz.mongodb.net/rammedb?retryWrites=true",
+  collection: "sessions"
+});
+
+store.on("error", function(error) {
+  console.log(error);
+});
 
 app.use(cors(corsOptions));
 
 app.use(
-    session({
-        secret: "Shh, its a secret!",
-        resave: false,
-        saveUninitialized: true
-    })
+  session({
+    secret: "Shh, its a secret!",
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+  })
 );
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    console.log(req.hostname);
-    res.redirect("http://" + req.hostname + ":5000");
+  console.log(req.hostname);
+  res.redirect("http://" + req.hostname + ":5000");
 });
 
 app.get("/api/messages", chatManager.getMessages);
@@ -44,11 +58,11 @@ app.post("/api/messages", chatManager.sendMessage);
 app.get("/api/session", sessionManager.getSession);
 
 app.get("/api/locations", (req, res) => {
-    res.send(temmplateData.locations);
+  res.send(temmplateData.locations);
 });
 
 app.get("/api/catchphrases", (req, res) => {
-    res.send(temmplateData.catchphrases);
+  res.send(temmplateData.catchphrases);
 });
 
 const port = process.env.PORT || 5000;
