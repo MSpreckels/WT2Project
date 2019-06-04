bdddcd9d4934becc3651f971b65678c913609e17
thekiddiegameisdownthestreet <fabian.ritter@hs-bochum.de>
2019-06-04 18:27:14 +0200

    changed logic from MongoClient to mongoose

--- ramme-backend/modules/partyManager.js
+++ ramme-backend/modules/partyManager.js
@@ -1,73 +1,68 @@
-const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017/";
+const url = "mongodb://localhost:27017/mongo-db-test";
 const ObjectID = require("mongodb").ObjectID;
+const mongoose = require("mongoose");
+const partySchema = {
+  location: String,
+  time: String,
+  members: [String]
+};
+const Parties = mongoose.model("parties", partySchema);
 
 async function addToParty(p_location, p_time, p_idMember) {
-    
-    let party = {};
+  let party = {};
 
-    MongoClient.connect(url, {useNewUrlParser: true}, 
-        function(err, db) 
-        {
-            if (err) throw err;
-        
-            const dbo = db.db("mongo-db-test");
+  mongoose.connect(url, { useNewUrlParser: true });
 
-            party = await dbo.collection("parties").findOneAndUpdate(
-                {
-                    location: p_location,
-                    time: p_time,
-                    $expr: { $lt: [{ $size: "$members" }, 4] }
-                },
-                { $push: { members: p_idMember } },
-                { returnOriginal: false }
-            );
-        
-            if (party.value == null) 
-            {
-                party = await dbo.collection("parties").insertOne(
-                {
-                    location: p_location,
-                    time: p_time,
-                    members: [p_idMember]
-                });
-            }
-            db.close();
-        }
-    );
-    return party.value;
+  party = await Parties.findOneAndUpdate(
+    {
+      location: p_location,
+      time: p_time,
+      $expr: { $lt: [{ $size: "$members" }, 4] }
+    },
+    { $push: { members: p_idMember } },
+    { new: true, useFindAndModify: false }
+  );
+
+  console.log(party);
+
+  if (party == null) {
+    party = await Parties.create({
+      location: p_location,
+      time: p_time,
+      members: [p_idMember]
+    });
+  }
+
+  return party;
 }
 
-
 async function deleteFromParty(p_idPartyHex, p_idMember) {
+  const objectIdParty = new ObjectID(p_idPartyHex);
+  let party = {};
+  mongoose.connect(url, { useNewUrlParser: true });
 
-    const objectIdParty = new ObjectID(p_idPartyHex);
-    let party = {};
+  party = await Parties.findOneAndUpdate(
+    {
+      _id: objectIdParty
+    },
+    {
+      $pull: { members: p_idMember }
+    },
+    {
+      new: true,
+      useFindAndModify: false
+    }
+  );
 
-    MongoClient.connect(url, {useNewUrlParser: true}, 
-        function(err, db) 
-        {
-            party = await dbo.collection("parties").findOneAndUpdate(
-                {
-                    _id: objectIdParty
-                },
-                {
-                    $pull: { members: p_idMember }
-                },
-                {
-                    returnOriginal: false
-                }
-            );
-            //console.log(delParty);
-            if (party.value.members.length == 0) 
-            {let delObject = await dbo.collection("parties").deleteOne({
-                    _id: objectIdParty
-                });
-            }
-            db.close();
-        }
-    );
-    return party;
+  console.log(party);
+
+  if (party.members.length == 0) {
+    let delObject = await dbo.collection("parties").deleteOne({
+      _id: objectIdParty
+    });
+  }
+
+  return party;
 }
 
 exports.addToParty = addToParty;
