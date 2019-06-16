6e6cd7eefcd2fcab6298e5329ad128a0b43bc67f
thekiddiegameisdownthestreet <fabian.ritter@hs-bochum.de>
2019-06-16 18:49:45 +0200

    added class-autobind to main

--- ramme/src/components/main.jsx
+++ ramme/src/components/main.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import autobind from "class-autobind";
 import Action from "./action";
 import Activity from "./activity";
 import Text from "./text";
@@ -6,89 +7,111 @@ import ApiManager from "../js/ApiManager";
 import "../css/main.css";
 
 class Main extends Component {
-    state = {
-        names: null,
-        activity: 0,
-        hasParty: false,
-        locations: [],
-        meetingTime: 0,
-        meetingLocation: "Mensa Hochschule Bochum",
-        catchphrases: null,
-        currentPartyMembers: ["","","",""]
+  constructor(props) {
+    super(props);
+    this.state = {
+      names: null,
+      activity: 0,
+      hasParty: false,
+      locations: [],
+      meetingTime: 0,
+      meetingLocation: "Mensa Hochschule Bochum",
+      catchphrases: null,
+      currentPartyMembers: ["", "", "", ""]
+    };
+    autobind(this);
+  }
+
+  onButtonClick = lastButtonState => {
+    const activity = this.state.activity === 0 ? 1 : 0;
+
+    if (lastButtonState === 0) this.searchForAvailableGroup(); //if last button state was start then we are looking for a party now
+    if (lastButtonState === 1) this.exitParty();
+    this.setState({ activity: activity });
+  };
+
+  onTimeChange = timeID => {
+    console.log(timeID);
+    this.setState({ meetingTime: timeID });
+  };
+
+  onLocationChange = event => {
+    this.setState({ meetingLocation: event.target.value });
+  };
+
+  searchForAvailableGroup() {
+    //check if there are parties available who arent full yet
+    //if none is found create a new party
+    //for debug purposes i'll use setTimeout
+    let am = new ApiManager("http://localhost:5000/api");
+    let currentTime = new Date().getTime();
+    let partyInfos = {
+      location: this.state.meetingLocation,
+      timeStart: currentTime,
+      timeEnd: currentTime + this.state.meetingTime * 60000
     };
 
-    onButtonClick = lastButtonState => {
-        const activity = this.state.activity === 0 ? 1 : 0;
+    //TODO: Fetch names of members
+    am.post("parties", partyInfos).then(res => {
+      let members = this.state.currentPartyMembers;
+      for (let i = 0; i < members.length; i++) {
+        members[i] = i < res.body.members.length ? res.body.members[i] : "...";
+      }
+      this.setState({ currentPartyMembers: members });
+    });
 
-        if (lastButtonState === 0) this.searchForAvailableGroup(); //if last button state was start then we are looking for a party now
-        if (lastButtonState === 1) this.exitParty(); 
-        this.setState({ activity: activity });
-    };
+    //TODO: update members using emit?
 
-    onTimeChange = timeID => {
-        console.log(timeID);
-        this.setState({ meetingTime: timeID });
-    };
+    //TODO: check if current party is full and set this.foundParty to true
+    //setTimeout(this.foundParty, 5000);
+  }
 
-    onLocationChange = event => {
-        this.setState({ meetingLocation: event.target.value });
-    };
+  exitParty() {
+    let am = new ApiManager("http://localhost:5000/api");
+    am.delete("parties").then(res => {
+      console.log("exited party");
+    });
+  }
 
-    searchForAvailableGroup() {
-        //check if there are parties available who arent full yet
-        //if none is found create a new party
-        //for debug purposes i'll use setTimeout
-        let am = new ApiManager("http://localhost:5000/api");
-        let currentTime = new Date().getTime();
-        let partyInfos = {location: this.state.meetingLocation, 
-                        timeStart: currentTime,
-                        timeEnd: currentTime + (this.state.meetingTime*60000)};
-        
-        //TODO: Fetch names of members
-        am.post("parties", partyInfos).then((res) => {
-            let members = this.state.currentPartyMembers;
-            for(let i = 0; i < members.length; i++)
-            {
-                members[i] = i < res.body.members.length ? res.body.members[i] : "...";
-            }
-            this.setState({currentPartyMembers: members});
-        });
+  foundParty = () => {
+    console.log("found a party! \\o/");
+    this.setState({ hasParty: true });
+  };
 
-        //TODO: update members using emit?
+  componentDidMount() {
+    //let am = new ApiManager("http://81.169.194.105:5000/api");
+    let am = new ApiManager("http://localhost:5000/api");
+    am.get("locations").then(res => this.setState({ locations: res.body }));
+    am.get("catchphrases").then(res =>
+      this.setState({ catchphrases: res.body.catchphrases })
+    );
+  }
 
-        //TODO: check if current party is full and set this.foundParty to true
-        //setTimeout(this.foundParty, 5000);
-    }
-
-    exitParty() {
-        let am = new ApiManager("http://localhost:5000/api");
-        am.delete("parties").then((res) => {
-            console.log("exited party");
-        });
-    }
-
-    foundParty = () => {
-        console.log("found a party! \\o/");
-        this.setState({ hasParty: true });
-    };
-
-    componentDidMount() {
-        
-        //let am = new ApiManager("http://81.169.194.105:5000/api");
-        let am = new ApiManager("http://localhost:5000/api");
-        am.get("locations").then(res => this.setState({ locations: res.body }));
-        am.get("catchphrases").then(res => this.setState({ catchphrases: res.body.catchphrases }));
-    }
-
-    render() {
-        return (
-            <main>
-                <Text phrases={this.state.catchphrases != null ? this.state.catchphrases : []} />
-                <Action onButtonClick={this.onButtonClick} buttonState={this.state.activity} hasParty={this.state.hasParty} meetingTime={this.state.meetingTime} meetingLocation={this.state.meetingLocation} />
-                <Activity activity={this.state.activity} currentPartyMembers={this.state.currentPartyMembers} locations={this.state.locations} onTimeChange={this.onTimeChange} onLocationChange={this.onLocationChange} />
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main>
+        <Text
+          phrases={
+            this.state.catchphrases != null ? this.state.catchphrases : []
+          }
+        />
+        <Action
+          onButtonClick={this.onButtonClick}
+          buttonState={this.state.activity}
+          hasParty={this.state.hasParty}
+          meetingTime={this.state.meetingTime}
+          meetingLocation={this.state.meetingLocation}
+        />
+        <Activity
+          activity={this.state.activity}
+          currentPartyMembers={this.state.currentPartyMembers}
+          locations={this.state.locations}
+          onTimeChange={this.onTimeChange}
+          onLocationChange={this.onLocationChange}
+        />
+      </main>
+    );
+  }
 }
 
 export default Main;
